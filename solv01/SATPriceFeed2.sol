2a7630cc7c1abec1f8b433c0433ff79bbd48de64
3109Dev <108624007+ad3109@users.noreply.github.com>
2023-04-21 15:51:59 -0500

    Update SATPriceFeed2.sol
    update with PAXG and Brent Oil

--- solv01/SATPriceFeed2.sol
+++ solv01/SATPriceFeed2.sol
@@ -1,15 +1,14 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * This pricefeed was deployed via TRBTC: Tx Hash: 0x894a50a822934d0ab1f4ea3ef70123a39f54a34a3921b26136b14bfab36ee520 
- */
+//  @Dev remember any contracts using LINK need to be funded after they are deployed
+//  after fund run requestMultiparameters 
 
-contract MultiWordConsumer is ChainlinkClient, ConfirmedOwner {
+
+contract MultiCommoditiesFeed is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
     bytes32 private jobId;
@@ -17,30 +16,25 @@ contract MultiWordConsumer is ChainlinkClient, ConfirmedOwner {
 
     // multiple params returned in a single oracle response
     uint256 public btc;
-    uint256 public usd;
-    uint256 public eur;
+    uint256 public gold;
+    uint256 public oil;
 
     event RequestMultipleFulfilled(
         bytes32 indexed requestId,
         uint256 btc,
-        uint256 usd,
-        uint256 eur
+        uint256 gold,
+        uint256 oil
     );
 
     /**
-     * @notice Initialize the link token and target oracle
-     * @dev The oracle address must be an Operator contract for multiword response
-     *
+     * @dev Initialize the link token and target oracle
+     * @dev The oracle address must be an operator contract for multiword 
+     * response/orchastration to run 
      *
      * Sepolia Testnet details:
-     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+     * LINK: 0x779877A7B0D9E8603169DdbD7836e478b4624789  (Payto contract)
      * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
      * jobId: 53f9755920cd451a8fe46f5087468395
-     *
-     * Goerlli Testnet details:
-     * Link Token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
-     * Oracle: 0xCC79157eb46F5624204f47AB42b3906cAA40eaB7 (Chainlink DevRel)
-     * jobId:  fcf4140d696d44b687012232948bdd5d  // AnyAPI JobID 
      *
      */
     constructor() ConfirmedOwner(msg.sender) {
@@ -49,53 +43,68 @@ contract MultiWordConsumer is ChainlinkClient, ConfirmedOwner {
         jobId = "53f9755920cd451a8fe46f5087468395";
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
-
     /**
-     * @notice Request mutiple parameters from the oracle in a single transaction
+     * @dev Request mutiple parameters from the oracle in a single transaction
+     * requestMultipleParameters function builds the Chainlink.Request 
+     * it tells the LINK oracle where to fetch the price in the json response. 
+     * The URLs and paths, in this contract call any public API as long as
+     * the URLs and paths are copacetic
      */
+     
     function requestMultipleParameters() public {
         Chainlink.Request memory req = buildChainlinkRequest(
             jobId,
             address(this),
             this.fulfillMultipleParameters.selector
         );
+        //feed 1 BTC
         req.add(
             "urlBTC",
-            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC"
+            "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD&api_key=a61fc21ec8d4ace963b1c38c3b9378ff7bac83507ae2b3b9036fc5dd98dd994d"
+            // "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"
+            // The BTC to USD price URL w/ my cryptocompare key
         );
         req.add("pathBTC", "BTC");
+        
+        // feed 2 Gold via PAXG
+         req.add(
+            "urlGOLD",
+            "https://min-api.cryptocompare.com/data/price?fsym=PAXG&tsyms=USD&api_key=a61fc21ec8d4ace963b1c38c3b9378ff7bac83507ae2b3b9036fc5dd98dd994d"
+         );
+         req.add("pathGOLD", "GOLD");
+
+        // feed 3 Oil
         req.add(
-            "urlUSD",
-            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
-        );
-        req.add("pathUSD", "USD");
-        req.add(
-            "urlEUR",
-            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR"
-        );
-        req.add("pathEUR", "EUR");
+            "urlOIL",
+            "https://commodities-api.com/api/latest?access_key=ho90ir8l3777dymv9s6vzblbt5p34udvu3bqub662168au23b963x0gzq7kg"
+         );
+         req.add("pathOIL", "OIL");
+         req.add("pathOIL", "x.data.rates.BRENTOIL");
+
         sendChainlinkRequest(req, fee); // MWR API.
     }
 
     /**
-     * @notice Fulfillment function for multiple parameters in a single request
-     * @dev This is called by the oracle. recordChainlinkFulfillment must be used.
+     * @dev Fulfillment function for multiple parameters in a single request
+     * This is called by the oracle. recordChainlinkFulfillment must be used.
      */
+
     function fulfillMultipleParameters(
         bytes32 requestId,
         uint256 btcResponse,
-        uint256 usdResponse,
-        uint256 eurResponse
-    ) public recordChainlinkFulfillment(requestId) {
+        uint256 goldResponse,
+        uint256 oilResponse
+         ) 
+    public recordChainlinkFulfillment(requestId) {
         emit RequestMultipleFulfilled(
             requestId,
             btcResponse,
-            usdResponse,
-            eurResponse
+            goldResponse,
+            oilResponse
         );
         btc = btcResponse;
-        usd = usdResponse;
-        eur = eurResponse;
+        gold = goldResponse;
+        oil = oilResponse;
     }
 
     /**
@@ -110,7 +119,7 @@ contract MultiWordConsumer is ChainlinkClient, ConfirmedOwner {
     }
 }
 /**
- * Request testnet TRBTC here: https://faucet.rsk.co/
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ * Request testnet LINK and ETH here: https://faucets.chain.link/  /  https://docs.chain.link/resources/acquire-link
+ * Find information on LINK Token Contracts and get the latest ETH + LINK faucets: https://docs.chain.link/docs/link-token-contracts/
+ * Ref: https://docs.chain.link/any-api/get-request/examples/multi-variable-responses/ 
  */
